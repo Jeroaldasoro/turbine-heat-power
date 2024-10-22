7690baece3ac87867a56a7fd454f6aa44a276e8a
Jeroaldasoro <82033830+Jeroaldasoro@users.noreply.github.com>
2024-10-22 15:06:25 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/turbine-heat-power',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
+  assetPrefix: '/turbine-heat-power/',
 }
 
 module.exports = nextConfig
